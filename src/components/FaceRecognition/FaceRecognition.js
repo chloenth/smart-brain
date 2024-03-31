1640cceeb8067645f85b1d531d40e6ccf2f8a934
Huong <huongnguyen.nth1709@gmail.com>
2024-03-31 13:24:18 +0300

    modify img's source and add css class bounding-box

--- src/components/FaceRecognition/FaceRecognition.js
+++ src/components/FaceRecognition/FaceRecognition.js
@@ -1,15 +1,26 @@
 import React from 'react';
+import './FaceRecognition.css';
 
-const FaceRecognition = () => {
+const FaceRecognition = ({ imageUrl, box }) => {
   return (
     <div className='center ma'>
       <div className='absolute mt2'>
         <img
-          src='https://ew.com/thmb/zHfRZwt6Dohbbp6jmlR8ciIz5lw=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/Jungkook-6837e9da24d24d428979a41d6df11012.jpg'
+          id='inputImage'
+          src={imageUrl && imageUrl}
           alt=''
           width='500px'
           height='auto'
         />
+        <div
+          className='bounding-box'
+          style={{
+            top: box.topRow,
+            right: box.rightCol,
+            bottom: box.bottomRow,
+            left: box.leftCol,
+          }}
+        ></div>
       </div>
     </div>
   );
