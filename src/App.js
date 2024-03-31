072392fffd8264bb7711a8c6009fba5b94e771fb
Huong <huongnguyen.nth1709@gmail.com>
2024-03-31 14:11:23 +0300

    implement logic to fetch Clariify AI-Model for face-detection

--- src/App.js
+++ src/App.js
@@ -1,18 +1,99 @@
+import { useState } from 'react';
 import './App.css';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
 import Logo from './components/Logo/Logo';
 import Navigation from './components/Navigation/Navigation';
 import Rank from './components/Rank/Rank';
+import ParticlesBg from 'particles-bg';
+import FaceRecognition from './components/FaceRecognition/FaceRecognition';
+import Signin from './components/Signin/Signin';
+
+const returnClarifyRequestOptions = (imageUrl) => {
+  // Your PAT (Personal Access Token) can be found in the Account's Security section
+  const PAT = process.env.PAT;
+  // Specify the correct user_id/app_id pairings
+  // Since you're making inferences outside your app's scope
+  const USER_ID = process.env.USER_ID;
+  const APP_ID = process.env.APP_ID;
+  // Change these to whatever model and image URL you want to use
+  const IMAGE_URL = imageUrl;
+
+  const raw = JSON.stringify({
+    user_app_id: {
+      user_id: USER_ID,
+      app_id: APP_ID,
+    },
+    inputs: [
+      {
+        data: {
+          image: {
+            url: IMAGE_URL,
+          },
+        },
+      },
+    ],
+  });
+
+  const requestOptions = {
+    method: 'POST',
+    headers: {
+      Accept: 'application/json',
+      Authorization: 'Key ' + PAT,
+    },
+    body: raw,
+  };
+
+  return requestOptions;
+};
 
 function App() {
+  const [imageUrl, setImageUrl] = useState('');
+  const [box, setBox] = useState({});
+
+  const onInputChange = (e) => {
+    setImageUrl(e.target.value);
+  };
+
+  const calculateFaceLocation = (data) => {
+    const clarifaiFace =
+      data.outputs[0].data.regions[0].region_info.bounding_box;
+    const image = document.getElementById('inputImage');
+    const width = Number(image.width);
+    const height = Number(image.height);
+
+    return {
+      leftCol: clarifaiFace.left_col * width,
+      topRow: clarifaiFace.top_row * height,
+      rightCol: width - clarifaiFace.right_col * width,
+      bottomRow: height - clarifaiFace.bottom_row * height,
+    };
+  };
+
+  const displayFaceBox = (box) => {
+    setBox(box);
+  };
+
+  const onSubmit = () => {
+    fetch(
+      'https://api.clarifai.com/v2/models/face-detection/outputs',
+      returnClarifyRequestOptions(imageUrl)
+    )
+      .then((response) => response.json())
+      .then((result) => {
+        displayFaceBox(calculateFaceLocation(result));
+      })
+      .catch((err) => console.log(err));
+  };
+
   return (
     <div className='App'>
+      <ParticlesBg type='fountain' bg={true} />
       <Navigation />
+      <Signin />
       <Logo />
       <Rank />
-      <ImageLinkForm />
-      {/* 
-      <FaceRecognition /> */}
+      <ImageLinkForm onInputChange={onInputChange} onSubmit={onSubmit} />
+      <FaceRecognition box={box} imageUrl={imageUrl} />
     </div>
   );
 }
