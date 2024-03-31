061a4827763e8685b4eb0d00923c55e8be6f5b2c
Huong <huongnguyen.nth1709@gmail.com>
2024-03-31 09:57:17 +0300

    create FaceRecognition component to display the image

---
+++ src/components/FaceRecognition/FaceRecognition.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const FaceRecognition = () => {
+  return (
+    <div className='center ma'>
+      <div className='absolute mt2'>
+        <img
+          src='https://ew.com/thmb/zHfRZwt6Dohbbp6jmlR8ciIz5lw=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/Jungkook-6837e9da24d24d428979a41d6df11012.jpg'
+          alt=''
+          width='500px'
+          height='auto'
+        />
+      </div>
+    </div>
+  );
+};
+
+export default FaceRecognition;
