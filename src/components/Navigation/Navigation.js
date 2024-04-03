33325bcbac06c90adf94d5c988f7292122ca89c7
Huong <huongnguyen.nth1709@gmail.com>
2024-04-03 15:46:01 +0300

    modify route when signout

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -6,7 +6,7 @@ const Navigation = ({ onRouteChange, isSignedIn }) => {
       <nav style={{ display: 'flex', justifyContent: 'flex-end' }}>
         <p
           className='f3 link dim black underline pa3 pointer'
-          onClick={() => onRouteChange('signout')}
+          onClick={() => onRouteChange('signin')}
         >
           Sign Out
         </p>
