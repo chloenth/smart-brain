60352893fd83ab80b9d7e35c4c6313795dbb4d17
Huong <huongnguyen.nth1709@gmail.com>
2024-04-03 15:40:20 +0300

    modify display text

--- src/components/Rank/Rank.js
+++ src/components/Rank/Rank.js
@@ -1,10 +1,12 @@
 import React from 'react';
 
-const Rank = () => {
+const Rank = ({ user }) => {
   return (
     <div>
-      <div className='white f3'>Andrei, your current rank is...</div>
-      <div className='white f1'>#5</div>
+      <div className='white f3'>
+        {user.name}, Your current entry count is...
+      </div>
+      <div className='white f1'>{user.entries}</div>
     </div>
   );
 };
